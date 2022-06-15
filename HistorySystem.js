3b36ac312d2fc9fdd9c541f5a6ba7e07356db431
lucas <lucas.lvn97439@gmail.com>
2022-06-15 20:28:55 +0400

    proxy config for history

---
+++ HistorySystem.js
@@ -0,0 +1,39 @@
+const gestionnaire = {
+    /**
+     * @summary When getter is called on cible execute
+     * @param {any} cible 
+     * @param {string} prop 
+     * @param {any} recepteur 
+     * @returns 
+     */
+    get(cible, prop, recepteur) {
+        recepteur
+        cible.history.last_called_method = prop
+        return Reflect.get(...arguments);
+    },
+
+    /**
+     * @summary When set property of object "obj" do next code
+     * @param {any} obj 
+     * @param {string} prop 
+     * @param {any} valeur 
+     * @returns 
+     */
+    set(obj, prop, valeur){
+        // console.log('--------', obj);
+        const hitem = {
+            id: obj.history.items.length + 1,
+            date: Date.now(),
+            prop: prop,
+            last_value: obj[prop],
+            new_value:  valeur,
+            active:     true
+        }
+        obj[prop]   = valeur;
+        obj.history.items.push(hitem);
+        return true
+    },
+}
+
+
+export default gestionnaire
