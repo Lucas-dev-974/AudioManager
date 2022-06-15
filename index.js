f24cf234f601a6494273e67dca014727b9a03ad4
lucas <lucas.lvn97439@gmail.com>
2022-06-15 20:30:29 +0400

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { smanager } from './player_.js'
+
+smanager.initPlayer()
+smanager.test()
+smanager.test('modification')
+smanager.test('LOLOTEST')
+smanager.test('TESRT')
+
+smanager.historyBackward()
+smanager.historyBackTo(1)
+
+// smanager.historyBackTo(3)
+
+console.log(smanager);
