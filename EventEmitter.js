252b30570f50e06693707eeeacb095df0f3700ce
lucas <lucas.lvn97439@gmail.com>
2022-07-08 22:47:57 +0400

    Setup event emitter class

---
+++ EventEmitter.js
@@ -0,0 +1,40 @@
+export default class EventEmit {
+    constructor() {
+        this._events = {};
+      }
+    
+      /**
+       * @summary check if event name exist in events 
+       * @param {*} name 
+       * @param {*} listener 
+       */
+      on(name, listener) {
+        if (!this._events[name]) {
+          this._events[name] = [];
+        }
+    
+        this._events[name].push(listener);
+      }
+    
+      removeListener(name, listenerToRemove) {
+        if (!this._events[name]) {
+          throw new Error(`Can't remove a listener. Event "${name}" doesn't exits.`);
+        }
+    
+        const filterListeners = (listener) => listener !== listenerToRemove;
+    
+        this._events[name] = this._events[name].filter(filterListeners);
+      }
+    
+      emit(name, data) {
+        if (!this._events[name]) {
+          throw new Error(`Can't emit an event. Event "${name}" doesn't exits.`);
+        }
+    
+        const fireCallbacks = (callback) => {
+          callback(data);
+        };
+    
+        this._events[name].forEach(fireCallbacks);
+      }
+}
