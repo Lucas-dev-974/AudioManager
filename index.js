d086fa077f339681ba206352da13e0ea9a8dd7ef
lucas <lucas.lvn97439@gmail.com>
2022-07-08 22:48:31 +0400

    index.js updated

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import { smanager } from './player_.js'
+import { smanager } from './Player.js'
 
-smanager.initPlayer()
-smanager.test()
-smanager.test('modification')
-smanager.test('LOLOTEST')
-smanager.test('TESRT')
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsInBhc3N3b3JkIjoiJDJiJDA4JEhrTkd4Y3cuYS5IQ0FuSkNSeUY4YmVzM3pMT210dk1qeVo2Y2ouV2QwUEpUNGZEUERYcWU2IiwiaWF0IjoxNjU3MjU2ODEwLCJleHAiOjE2NTc2ODg4MTB9.a4jCRvT-diyXzNnwOMSKIzrE-pSjEkvMccYk58cDT9M"
+const url   = 'http://localhost:3000/api/pist/1?token=' + token
 
-smanager.historyBackward()
-smanager.historyBackTo(1)
+const tracks = [
+    {
+        src: url,
+        name: 'track 1'
+    }
+]
 
-// smanager.historyBackTo(3)
-
+smanager.laodTracks(tracks)
+smanager.addTrack(tracks[0])
 console.log(smanager);
